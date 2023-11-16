b462fb118c3fd3120b5c82179b3bb838674ac479
Petri Nurmi <pete874@hotmail.com>
2023-11-16 10:27:23 +0200

    More progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import api from './api/axiosConfig.js'
 import { useState, useEffect  } from 'react';
+import Layout from './components/Layout.js';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home.js';
+import Header from './components/header/Header.js';
+import Trailer from './components/trailer/Trailer.js';
 
 function App() {
 
@@ -25,6 +30,13 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path='/' element={<Home movies={movies}/>}></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}></Route>
+        </Route>
+      </Routes>
 
     </div>
   );
---
+++ src/components/header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
+import { Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { NavbarBrand, NavbarCollapse, NavbarToggle } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+            <NavbarBrand href="/" style={{"color":'gold'}}>
+                <FontAwesomeIcon icon={faVideoSlash}/>Gold
+            </NavbarBrand>
+            <NavbarToggle aria-controls='navbarScroll' />
+            <NavbarCollapse id='navbarScroll'>
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{maxHeight: '100px'}}
+                    navbarScroll
+                >
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/watchList">Watch list</NavLink>                 
+                </Nav>
+                <Button variant="outline-info" className="me-2">Login</Button>
+                <Button variant="outline-info" className="me-2">Register</Button>
+            </NavbarCollapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
